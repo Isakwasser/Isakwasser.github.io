2495dc01151974ba58284d0dd90f01fe76b61c20
Лебедев Андрей Алексеевич <a.lebedev@els24.com>
2022-02-06 21:31:40 +0300

    Добавил страницу ошибки

---
+++ router/src/components/pages/app404/app404.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="app404">
+    <div class="box text-center">
+      <h1 class="title">ОШИБКА 404</h1>
+      <h5 class="undertitle">Страницы не существует</h5>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "app404",
+};
+</script>
+
+<style lang="scss">
+.app404 {
+  width: 100vw;
+  height: 100vh;
+  background-color: #fbb;
+  background: radial-gradient(#f99, #f99fff);
+  .box {
+    position: absolute;
+    top: 50vh;
+    left: 50vw;
+    transform: translate(-50%, -50%);
+  }
+}
+</style>
