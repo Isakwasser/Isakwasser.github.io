6bbb3a77594809533d21e5fb50c5eea2a9a4c8ec
Лебедев Андрей Алексеевич <a.lebedev@els24.com>
2022-02-06 21:31:29 +0300

    Добавил страницу ошибки

--- router/src/router/index.js
+++ router/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Main from '@/components/Main'
 import appSnow from '@/components/pages/appSnow/appSnow.vue'
+import app404 from '@/components/pages/app404/app404'
 
 Vue.use(Router)
 
@@ -16,6 +17,10 @@ export default new Router({
       path: '/snow',
       name: 'appSnow',
       component: appSnow
-    }
+    }, {
+      path: '*',
+      name: 'app404',
+      component: app404
+    },
   ]
 })
