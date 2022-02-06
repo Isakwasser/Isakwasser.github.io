9a9c987dc8ac68d3a5a04cfa5d9d73a52acb8935
Лебедев Андрей Алексеевич <a.lebedev@els24.com>
2022-02-06 14:51:20 +0300

    Add project

---
+++ router/src/components/Main.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="main">
+    <h1>Главная страница</h1>
+    <router-link :to="{ name: 'appSnow' }"
+      >Перейти на страницу снега</router-link
+    >
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Main",
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
---
+++ router/src/components/pages/appSnow/appSnow.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="app-snow">
+    <h1>Страница appSnow</h1>
+    <router-link :to="{ name: 'Main' }">Перейти на главную</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "appSnow",
+};
+</script>
+
