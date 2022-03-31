062708d9165803da3ee8237bf4bf3ed8e6c0e46f
Лебедев Андрей Алексеевич <a.lebedev@els24.com>
2022-03-31 10:36:06 +0400

    Add new

--- fullstack/server/index.js
+++ fullstack/server/index.js
@@ -1,11 +1,10 @@
-// const dotenv = require('dotenv');
-// dotenv.config();
+const dotenv = require("dotenv");
+dotenv.config();
 
 // console.log(process.pid);
 // console.log(process.env.PORT);
 // console.log(process.argv);
 // process.exit();
-
 
 // const path = require('path');
 // console.log(path.join(__dirname,'first', 'second'));
@@ -87,3 +86,97 @@
 //     .then(data => console.log(data))
 //     .catch(err => console.log(err));
 
+// const os = require("os");
+// const cluster = require("cluster");
+
+// // console.log(os.platform());
+// // console.log(os.arch());
+// // console.log(os.cpus());
+
+// if (cluster.isMaster) {
+//   for (let i = 0; i < os.cpus().length - 2; i++) {
+//     cluster.fork();
+//   }
+//   cluster.on("exit", (worker, code, signal) => {
+//     console.log(`Воркер с pid= ${worker.process.pid} умер`);
+//     cluster.fork();
+//   });
+// } else {
+//   console.log(`Воркер с pid= ${process.pid} запущен`);
+
+//   setInterval(() => {
+//     console.log(`Воркер с pid= ${process.pid} еще работает`);
+//   }, 5000);
+// }
+
+// const Emitter = require("events");
+
+// const emitter = new Emitter();
+
+// // emitter.on("message", (data, second, third) => {
+// //   console.log(`Вы прислали сообщение ${data}`);
+// //   console.log(`Вы прислали сообщение ${second}`);
+// // });
+// emitter.once("message", (data, second, third) => {
+//   console.log(`Вы прислали сообщение ${data}`);
+//   console.log(`Вы прислали сообщение ${second}`);
+// });
+
+// const MESSAGE = process.env.MESSAGE || "";
+// if (MESSAGE) {
+//   emitter.emit("message", MESSAGE, 123);
+// }
+
+// /**
+//  * Streams
+//  * Readable
+//  * Writable
+//  * Duplex
+//  * Transform
+//  */
+
+// const fs = require("fs");
+// const path = require("path");
+
+// // fs.readFile(path.resolve(__dirname, "test.txt"), (err, data) => {
+// //   if (err) {
+// //     throw err;
+// //   }
+// //   console.log(data);
+// // });
+
+// const stream = fs.createReadStream(path.resolve(__dirname, "test.txt"));
+
+// stream.on("data", (chunk) => {
+//   console.log(chunk);
+// });
+
+// stream.on("end", () => console.log(`Закончили читать`));
+// stream.on("open", () => console.log(`Начали читать`));
+// stream.on("error", (e) => console.log(e));
+
+const http = require("http");
+
+const PORT = process.env.PORT || 5000;
+
+// const server = http.createServer((req, res) => {
+//   res.writeHead(200, {
+//     "Content-type": "text/html; charset=utf-8",
+//   });
+//   res.end("<h1>Сервер работает!</h1>");
+// });
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {
+    "Content-Type": "application/json",
+  });
+  if (req.url === "/users") {
+    return res.end(JSON.stringify([{ id: 1, name: "USERS" }]));
+  }
+  if (req.url === "/posts") {
+    return res.end("POSTS");
+  }
+  res.end("<h1>Сервер работает!</h1>");
+});
+
+server.listen(PORT, () => console.log(`Server started on PORT ${PORT}`));
